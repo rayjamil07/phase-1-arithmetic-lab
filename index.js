2a248c798122c46fb86206aff6e0136810c8bf4f
rayjamil <ryanwekesa019@gmail.com>
2024-11-26 13:02:58 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+let random =  Math.floor(Math.random())*1000 + 1;
+let num3 = 10;
+let num4 = 6;
+let mod = num3%num4;
+let max = Math.max(18,17,20);
 
-// Write your code here
+
+
+
